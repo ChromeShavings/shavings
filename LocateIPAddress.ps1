beb824223721a631a0f3e977f50e3ecd7ada5f71
ChromeShavings <113253306+ChromeShavings@users.noreply.github.com>
2022-09-11 20:08:27 -0500

    Changed Weather API's and formatting
    Changed weather API's from api.weather.gov to api.openweathermap.org. This required a sign-up in order to use the service. It is much more reliable with more geolookups available.

--- LocateIPAddress.ps1
+++ LocateIPAddress.ps1
@@ -16,6 +16,7 @@ Param
 )
 
 $JsonFile = $Null
+$WeatherAPIKey = '146782f9715ff38f82d32b891dae975e' #Only 60 requests per minute and 1 million calls a month (free) tier
 $ipv6Pattern = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
 $ipv4Pattern = '^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31))(?<!127)(?<!^10)(?<!^0)\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!192\.168)(?<!172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31))\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!\.0$)(?<!\.255$)$'
 
@@ -34,20 +35,21 @@ else {
 
 
     try {#Geolocation Info
-    $Geolocation_Info = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$IPAddress" | Select *
-    $Timezone = (Invoke-WebRequest "http://worldtimeapi.org/api/$($Geolocation_Info.timezone)")
-    $TimezoneAbbreviation = ((Invoke-WebRequest "http://worldtimeapi.org/api/$($Geolocation_Info.timezone)") | ConvertFrom-Json).abbreviation
+    Write-Host "Attempting Gelocation lookup using API site - http://ip-api.com/json" -ForegroundColor Cyan
+    $GeolocationInfo = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$IPAddress" | Select *
+    $Timezone = (Invoke-WebRequest "http://worldtimeapi.org/api/$($GeolocationInfo.timezone)")
+    $TimezoneAbbreviation = ((Invoke-WebRequest "http://worldtimeapi.org/api/$($GeolocationInfo.timezone)") | ConvertFrom-Json).abbreviation
     $DateTime =  ($Timezone | ConvertFrom-Json).datetime.Substring(0,16)
-    $Longitude = $Geolocation_Info.lon
-    $Latitude = $Geolocation_Info.lat
-    $Country = $Geolocation_Info.country
-    $City = $Geolocation_Info.city
-    $State = $Geolocation_Info.regionName
-    $Zipcode = $Geolocation_Info.zip
-    $Timezone = $Geolocation_Info.timezone
-    $ISP = $Geolocation_Info.isp
-    $ASOwner = $Geolocation_Info.as
-    $BlockOwner = $Geolocation_Info.org
+    $Longitude = $GeolocationInfo.lon
+    $Latitude = $GeolocationInfo.lat
+    $Country = $GeolocationInfo.country
+    $City = $GeolocationInfo.city
+    $State = $GeolocationInfo.regionName
+    $Zipcode = $GeolocationInfo.zip
+    $Timezone = $GeolocationInfo.timezone
+    $ISP = $GeolocationInfo.isp
+    $ASOwner = $GeolocationInfo.as
+    $BlockOwner = $GeolocationInfo.org
     }
 
     catch {
@@ -68,14 +70,14 @@ else {
     
     
         try {#Geoweather
-
-        $Geoweather = (Invoke-WebRequest -Method Get "https://api.weather.gov/points/$($Latitude),$($Longitude)" -ErrorAction SilentlyContinue | ConvertFrom-Json).properties.forecast
-        $WeatherNow = (Invoke-WebRequest -Method Get $GeoWeather | ConvertFrom-Json).properties.periods[0]
-        $CurrentTemperature = $WeatherNow.temperature
-        $CurrentForecast = $WeatherNow.shortForecast
-        $TemperatureUnit = $WeatherNow.temperatureUnit
-        $Windspeed = $WeatherNow.windSpeed
-        $WindDirection = $WeatherNow.windDirection
+        Write-Host "Attempting Geoweather lookup using API site https://api.openweathermap.org/data/2.5/weather?" -ForegroundColor Cyan
+        $Geoweather = (Invoke-WebRequest -Method Get "https://api.openweathermap.org/data/2.5/weather?lat=$($GeolocationInfo.lat)&lon=$($GeolocationInfo.lon)&appid=$($WeatherAPIKey)&units=imperial" | ConvertFrom-Json)
+        $CurrentTemperature = $Geoweather.main.temp
+        $HighToday = $Geoweather.main.temp_max
+        $LowToday = $Geoweather.main.temp_min
+        $CurrentForecast = ($Geoweather.weather.main) + '/' + ($Geoweather.weather.description)
+        $Windspeed = $Geoweather.wind.speed
+        $WindDirection = $Geoweather.wind.deg
         }
 
         catch {
@@ -83,8 +85,9 @@ else {
         Write-Host $_
 
         $CurrentTemperature = 'N/A'
+        $HighToday = 'N/A'
+        $LowToday = 'N/A'
         $CurrentForecast = 'N/A'
-        $TemperatureUnit = 'N/A'
         $Windspeed = 'N/A'
         $WindDirection = 'N/A'
         }
@@ -107,10 +110,11 @@ else {
             Write-Host "$Country is the country" -ForegroundColor Green
             Write-Host "$Timezone is the Time Zone" -ForegroundColor Green
             Write-Host "$DateTime $TimezoneAbbreviation is the Date/Time" -ForegroundColor Green
-            Write-Host "The temperature is $CurrentTemperature" -ForegroundColor Yellow
-            Write-Host "The temperature unit is $TemperatureUnit" -ForegroundColor Yellow
+            Write-Host "The current temperature (in Fahrenheit) is $CurrentTemperature" -ForegroundColor Yellow
+            Write-Host "The high for today (in Fahrenheit) is $HighToday" -ForegroundColor Yellow
+            Write-Host "The low for today (in Fahrenheit) is $LowToday" -ForegroundColor Yellow
             Write-Host "The current forecast is $CurrentForecast" -ForegroundColor Yellow
-            Write-Host "Wind Speeds traveling $WindDirection at $Windspeed" -ForegroundColor Yellow
+            Write-Host "Wind Speeds are traveling $WindDirection degrees at $Windspeed mph" -ForegroundColor Yellow
             Write-Host "$ISP is the ISP" -ForegroundColor Green
             Write-Host "$ASOwner owns the AS Number" -ForegroundColor Green
             Write-Host "$BlockOwner is the IP Block Owner" -ForegroundColor Green
@@ -118,7 +122,7 @@ else {
             if ($OutputtoJson)
             {
             Write-Host "Writing data to JSON file..."
-            $JsonFile = ($IPAddress, $TotalHops, $AverageLatency, $Latitude, $Longitude, $City, $State, $Country, $Timezone, $TimezoneAbbreviation, $DateTime, $CurrentTemperature, $TemperatureUnit, $Windspeed, $WindDirection, $ISP, $ASOwner, $BlockOwner) | ConvertTo-Json | Out-File -FilePath $OutputtoJson -Force
+            $JsonFile = ($IPAddress, $TotalHops, $AverageLatency, $Latitude, $Longitude, $City, $State, $Country, $Timezone, $TimezoneAbbreviation, $DateTime, $CurrentTemperature, $HighToday, $LowToday, $Windspeed, $WindDirection, $ISP, $ASOwner, $BlockOwner) | ConvertTo-Json | Out-File -FilePath $OutputtoJson -Force
             }
 
         }#eof Geoweather finally
@@ -128,7 +132,3 @@ else {
 }#eof else
 
 }#eof function
-
-      
-
-
