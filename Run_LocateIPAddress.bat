e4210abd0a4a7f1b34f0b640afe598756af76529
ChromeShavings <113253306+ChromeShavings@users.noreply.github.com>
2022-09-21 09:11:36 -0500

    Batch file used to securely open/execute LocateIPAddress.ps1
    This batch file is created to launch the .ps1 without having to open and execute it through its main interface. It will launch the .ps1 with -RemoteSigned permissions and allows the user to define parameters. An example is also defined for the user.

---
+++ Run_LocateIPAddress.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+REM This batch file is used to securely launch the LocateIPAddress.ps1 PowerShell script. 
+
+echo "NOTE: The batch file, Run_LocateIPAddress.bat, must reside within the same directory as the PowerShell script for it to run properly"
+
+echo "Example of how to call these functions in the script: IPAddress-Lookup -IPAddress 1.1.1.1 -OutputtoJson C:\FolderOfYourChoosing\output.json
+
+Powershell.exe -NoExit -NoLogo -executionpolicy remotesigned -File .\LocateIPAddress.ps1
