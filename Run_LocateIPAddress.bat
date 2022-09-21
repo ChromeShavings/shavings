3d8928e8fd654f7848ec4d86fee2f676e163b0aa
ChromeShavings <113253306+ChromeShavings@users.noreply.github.com>
2022-09-21 09:14:34 -0500

    Grammatical Fixes

--- Run_LocateIPAddress.bat
+++ Run_LocateIPAddress.bat
@@ -3,6 +3,6 @@ REM This batch file is used to securely launch the LocateIPAddress.ps1 PowerShel
 
 echo "NOTE: The batch file, Run_LocateIPAddress.bat, must reside within the same directory as the PowerShell script for it to run properly"
 
-echo "Example of how to call these functions in the script: IPAddress-Lookup -IPAddress 1.1.1.1 -OutputtoJson C:\FolderOfYourChoosing\output.json
+echo "Example of how to call these functions/parameters in the script: IPAddress-Lookup -IPAddress 1.1.1.1 -OutputtoJson C:\FolderOfYourChoosing\output.json
 
 Powershell.exe -NoExit -NoLogo -executionpolicy remotesigned -File .\LocateIPAddress.ps1
